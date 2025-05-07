b9712479e1f684220a824f7a140b7895e9ba161e
j-ordep <joaoppinheiro2005@email.com>
2025-05-07 16:51:56 -0300

    fix: add 'error' return to repository save method

--- GO-GATEWAY-API/internal/domain/errors.go
+++ GO-GATEWAY-API/internal/domain/errors.go
@@ -3,8 +3,15 @@ package domain
 import "errors"
 
 var (
-	ErrAccountNotFound = errors.New("account not found") // é retornado quando a conta não é encontrada
-	ErrDuplicatedAPIKey = errors.New("api key already exists") // é retornado quando há tentativa de criar conta com API key duplciada 
-	ErrInvoiceNotFound = errors.New("invoice not found") // é retornado quando uma fatura não é encontrada
-	ErrUnauthorizedAccess = errors.New("unauthorized access") // é retornado quando há tentativa de acesso não altorizado a um recurso
+	// é retornado quando a conta não é encontrada
+	ErrAccountNotFound = errors.New("account not found") 
+
+	// é retornado quando há tentativa de criar conta com API key duplciada 
+	ErrDuplicatedAPIKey = errors.New("api key already exists") 
+
+	// é retornado quando uma fatura não é encontrada
+	ErrInvoiceNotFound = errors.New("invoice not found") 
+
+	// é retornado quando há tentativa de acesso não altorizado a um recurso
+	ErrUnauthorizedAccess = errors.New("unauthorized access") 
 )
