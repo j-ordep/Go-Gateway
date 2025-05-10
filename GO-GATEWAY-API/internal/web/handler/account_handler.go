ddf2f9104d8aecd731d525485a30d5e780ad2e54
j-ordep <joaoppinheiro2005@email.com>
2025-05-10 01:04:55 -0300

    feat: implement HTTP handler for account creation using JSON input

---
+++ GO-GATEWAY-API/internal/web/handler/account_handler.go
@@ -0,0 +1,36 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/j-ordep/gateway/go-gateway/internal/dto"
+	"github.com/j-ordep/gateway/go-gateway/internal/service"
+)
+
+type AccountHandler struct {
+	accountService *service.AccountService
+}
+
+func NewAccountHandler(accountService *service.AccountService) *AccountHandler {
+	return &AccountHandler{accountService: accountService}
+}
+
+func (h *AccountHandler) Create(w http.ResponseWriter, r *http.Request) {
+	var input dto.CreateAccountInput
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	} 
+
+	output, err := h.accountService.CreateAccount(input)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return  
+	}
+
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(output)
+}
