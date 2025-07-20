72eb655c9d8710cf3512b1c3c8257825cb5fe01d
j-ordep <joaoppinheiro2005@gmail.com>
2025-07-19 22:02:40 -0300

    feat: add web server

---
+++ GO-GATEWAY-API/internal/web/server/server.go
@@ -0,0 +1,39 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/j-ordep/gateway/go-gateway/internal/service"
+	"github.com/j-ordep/gateway/go-gateway/internal/web/handler"
+)
+
+type Server struct {
+	router *chi.Mux
+	server *http.Server
+	accountService *service.AccountService
+	port string
+}
+
+func NewServer(accountService *service.AccountService, port string) *Server {
+	return &Server{
+		router: chi.NewRouter(),
+		accountService: accountService,
+		port: port,
+	}
+}
+
+func (s *Server) ConfigureRoute() {
+	accountHandler := handler.NewAccountHandler(s.accountService)
+
+	s.router.Post("/accounts", accountHandler.Create)
+	s.router.Get("/accounts", accountHandler.Get)
+}
+
+func (s *Server) Start() error {
+	s.server = &http.Server{
+		Addr: ":" + s.port,
+		Handler: s.router,
+	}
+	return s.server.ListenAndServe()
+}
